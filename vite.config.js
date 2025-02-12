de27fd5de174d09003bcdc84025c084de91c9e62
arg43798 <arg43798@email.vccs.edu>
2025-02-11 22:42:39 -0500

    publish

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+    base: "/Week3Project/",
     build: {
         outDir: "docs",
         rollupOptions: {
